0b14cd32bb081a59858a0104b4bbe8f20f2166eb
John Wilkey <jwilkey@geomartin.com>
2014-06-09 13:48:27 -0700

    Added Versioning Information To Assembly

---
+++ AutoRDrive/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoRDrive")]
+[assembly: AssemblyDescription("Automatic RDrive Instatiation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("George Martin Company")]
+[assembly: AssemblyProduct("AutoRDrive.Properties")]
+[assembly: AssemblyCopyright("2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c83dc510-ce08-4b42-9d24-690c460002cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
