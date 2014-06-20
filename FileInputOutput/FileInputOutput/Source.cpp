d807f5f949937b486a6e61e8952734ac7a2a6582
John Wilkey <jwilkey@geomartin.com>
2014-06-20 13:05:53 -0700

    Fixed support for XP and bug in 2008,2003

--- FileInputOutput/FileInputOutput/Source.cpp
+++ FileInputOutput/FileInputOutput/Source.cpp
@@ -2,18 +2,22 @@
 // on disk. 
 #include<Windows.h>
 #include<tchar.h>
+#include<stdio.h>
+#include<fstream>
+#include<iostream>
+void writeFile(wchar_t*, wchar_t*);
 
 int __cdecl _tmain()
 {
     TCHAR filename[] = _T("\\\\backups\\computerimagingprimary\\resources\\ImageCreation.log");
     TCHAR name[32767];
     DWORD nameSize = 32767;      
+    DWORD bytesWritten;
     SYSTEMTIME time;
-    TCHAR data[sizeof(TCHAR) * 1024];
-    TCHAR result[] = _T("Result: SUCCESS\r\n");
+    TCHAR data[sizeof(TCHAR) * 1024] = _T("");
+    TCHAR result[] = _T("Result: FAILED!!\r\n");
     GetSystemTime(&time);                            
     GetComputerName(&(*name), &nameSize);
-
     _stprintf_s(
         data, 
         _T("%2.2i-%2.2i-%4.4i, %2.2i-%2.2i: "), 
@@ -27,7 +31,7 @@ int __cdecl _tmain()
     _tcscat_s(data, &(*name));
     _tcscat_s(data, _T(", "));
     _tcscat_s(data, result);
-    
+
     HANDLE file = CreateFile(
         filename,
         FILE_APPEND_DATA,
@@ -37,8 +41,22 @@ int __cdecl _tmain()
         FILE_ATTRIBUTE_NORMAL,
         NULL
     );
-
-    if (file == INVALID_HANDLE_VALUE) { return; }
-    WriteFile(file, data, _tcslen(data)*sizeof(TCHAR), NULL, NULL);
+    wprintf(L"Writing %s\nTo %s\n", data, filename);
+    
+    if (file == INVALID_HANDLE_VALUE) { 
+        wprintf(L"File handle was invalid");    
+        return EXIT_FAILURE; 
+    }
+    WriteFile(file, data, _tcslen(data)*sizeof(TCHAR), &bytesWritten, NULL);    
+    wprintf(L"Wrote %d bytes, closing file and exiting\n", bytesWritten);
     CloseHandle(file);
+    return EXIT_SUCCESS;
+}
+
+void writeFile(wchar_t* filename, wchar_t* message)
+{
+    std::wofstream file;
+    file.open(filename);
+    file << message;
+    file.close();
 }
